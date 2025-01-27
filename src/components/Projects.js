69b2107986f75cf26243a75c698e5c886ed55266
usama644515 <hanifusama688@gmail.com>
2025-01-27 10:08:07 +0500

    change the project images

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,27 +1,28 @@
 /* eslint-disable @next/next/no-img-element */
 import styles from "./Projects.module.css";
+// import mobileapp from '@/assets/images/project/mobielapp.jpg';
 
 export default function Projects() {
   const services = [
     {
       title: "Application Development",
       description: "Design and develop user-friendly, scalable, and high-performance applications tailored to your business needs.",
-      image: "https://wheat-jellyfish-990458.hostingersite.com/wp-content/uploads/2024/06/Portfolio-1.jpg", // Add your image path
+      image: 'https://firebasestorage.googleapis.com/v0/b/innovative-bit-solutions.appspot.com/o/oriole%20energy%2Fmobile-app.jpg?alt=media&token=629870d8-b0e2-414e-a651-8059edc35545', // Add your image path
     },
     {
       title: "Website Development",
       description: "Create modern, responsive, and visually appealing websites to enhance your online presence and engage users effectively.",
-      image: "https://wheat-jellyfish-990458.hostingersite.com/wp-content/uploads/2024/06/Portfolio-2.jpg", // Add your image path
+      image: "https://firebasestorage.googleapis.com/v0/b/innovative-bit-solutions.appspot.com/o/oriole%20energy%2Fwebsite.jpg?alt=media&token=772090b5-62c2-4cbb-8319-985d2a2360bf", // Add your image path
     },
     {
       title: "Software Development",
       description: "Build robust, custom software solutions designed to streamline operations and drive efficiency for your organization.",
-      image: "https://wheat-jellyfish-990458.hostingersite.com/wp-content/uploads/2024/06/Portfolio-3.jpg", // Add your image path
+      image: "https://firebasestorage.googleapis.com/v0/b/innovative-bit-solutions.appspot.com/o/oriole%20energy%2Fsoftware.jpg?alt=media&token=1d99c54d-3e8a-4cbb-ae05-9c1ee420c725", // Add your image path
     },
     {
       title: "Network Solutions",
       description: "Provide comprehensive network solutions to your organization's services, including setup, management, and optimization, ensuring seamless operations and security.",
-      image: "https://wheat-jellyfish-990458.hostingersite.com/wp-content/uploads/2024/06/Portfolio-4.jpg", // Add your image path
+      image: "https://firebasestorage.googleapis.com/v0/b/innovative-bit-solutions.appspot.com/o/oriole%20energy%2Fnetworking.jpg?alt=media&token=c633f11f-a565-4e3b-87d7-de2e6b5e047d", // Add your image path
     },
     
     // {
